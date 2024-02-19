8d2117dee5730035fbc99e29d84f90b95bebae3f
Thereache <thereache2003@gmail.com>
2024-02-20 00:20:28 +0530

    stone: treck the common kernel
    Signed-off-by: Thereache <thereache2003@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+git clone --depth=1 https://github.com/Thereache/vendor_xiaomi_stone_unified.git -b 14 vendor/xiaomi
+
+git clone --depth=1 https://github.com/Thereache/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375
+
+git clone --depth=1 https://github.com/Thereache/device_xiaomi_stone-kernel.git -b aosp-14 device/xiaomi/stone-kernel
+
+git clone --depth=1 https://github.com/Thereache/Stone_hardware_xiaomi.git hardware/xiaomi
